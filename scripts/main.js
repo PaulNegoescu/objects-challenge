48ca9fc4c0c1c52776b210c9226c9e7812dd58ed
Paul Negoescu <paul.negoescu@scoalainformala.ro>
2023-03-16 14:29:38 +0200

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+// Sa se ia toate planetele de la server
+// Planetele sa fie grupate dupa tipul de climat, atentie unele au doua 
+// sau mai multe climate separate prin virgula si spatiu
+// Sa fie afisat numele planetelor intr-un <a> intr-o lista, fiecare lista
+// sa corespunda unui climat, numele climatului sa fie un <h2>
+// Per climat sa se calculeze populatia totala a planetelor si sa fie afisata ca si <h3>
+// sub <h2>
+// Planeta din climatul curent cu perioada orbitala cea mai mare sa fie cu textul rosu
+// <a>-ul sa duca la o alta pagina unde sa se afiseze toate personajele care au legatura cu
+// planeta pe care s-a dat click (numele personajelor)
